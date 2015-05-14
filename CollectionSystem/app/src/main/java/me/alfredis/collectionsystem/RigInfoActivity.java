cf254b0998b4ed05ca9100606b7e7b70e56a02d7
Alfred Shi <memoriedsea@gmail.com>
2015-05-14 12:04:25 +0800

    add spinner logics

--- CollectionSystem/app/src/main/java/me/alfredis/collectionsystem/RigInfoActivity.java
+++ CollectionSystem/app/src/main/java/me/alfredis/collectionsystem/RigInfoActivity.java
@@ -4,15 +4,40 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import me.alfredis.collectionsystem.R;
+import me.alfredis.collectionsystem.datastructure.Hole;
+import me.alfredis.collectionsystem.datastructure.RigEvent;
 
 public class RigInfoActivity extends ActionBarActivity {
+
+    private RigEvent rig;
+    private String requestCode;
+
+    private EditText classPeopleCountEditText;
+
+    private Button addRigButton;
+
+    private Spinner rigTypeSpinner;
+
+    private static final String[] PROJECT_ID_PART1_SPINNER_OPTIONS = {"搬家移孔、下雨停工，其他",
+            "干钻、合水钻、金刚石钻及其他", "标准贯入试验", "动力触探试验", "下套管"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rig_info);
+
+        classPeopleCountEditText = (EditText) findViewById(R.id.class_people_count);
+
+        addRigButton = (Button) findViewById(R.id.button_add_rig);
+
+        rigTypeSpinner = (Spinner) findViewById(R.id.spinner_rig_type);
+
+
     }
 
     @Override
