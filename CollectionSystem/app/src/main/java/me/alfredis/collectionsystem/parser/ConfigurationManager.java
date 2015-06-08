b6f7207075c832f6fd4251baed088ad556ddef0e
Alfred Shi <memoriedsea@gmail.com>
2015-06-08 16:25:54 +0800

    initialize the configuration manager

---
+++ CollectionSystem/app/src/main/java/me/alfredis/collectionsystem/parser/ConfigurationManager.java
@@ -0,0 +1,275 @@
+package me.alfredis.collectionsystem.parser;
+
+/**
+ * Created by liashi on 2015/6/8.
+ */
+public class ConfigurationManager {
+    private static double sptTable1Argument1 = 2;
+    private static double sptTable1Argument2 = 4;
+    private static double sptTable1Argument3 = 7;
+    private static double sptTable1Argument4 = 18;
+    private static double sptTable1Argument5 = 35;
+
+    private static double sptTable2Argument1 = 10;
+    private static double sptTable2Argument2 = 15;
+    private static double sptTable2Argument3 = 30;
+
+    private static double sptTable3Argument1 = 10;
+    private static double sptTable3Argument2 = 15;
+    private static double sptTable3Argument3 = 30;
+
+    private static double dstTable1_63_5Argument1= 0;
+    private static double dstTable1_63_5Argument2= 5;
+    private static double dstTable1_63_5Argument3 = 10;
+    private static double dstTable1_63_5Argument4 = 20;
+
+    private static double dstTable1_120_Argument1 = 0;
+    private static double dstTable1_120_Argument2 = 3;
+    private static double dstTable1_120_Argument3 = 6;
+    private static double dstTable1_120_Argument4 = 11;
+    private static double dstTable1_120_Argument5 = 14;
+
+    private static double dstTable2_63_5Argument2 = 5;
+    private static double dstTable2_63_5Argument3 = 8;
+    private static double dstTable2_63_5Argument4 = 10;
+
+    private static double dstTable2_63_6Argument1 = 5;
+    private static double dstTable2_63_6Argument2 = 6.5;
+    private static double dstTable2_63_6Argument3 = 9.5;
+
+    private static double dstTable2_63_7Argument1 = 5;
+    private static double dstTable2_63_7Argument2 = 6;
+    private static double dstTable2_63_7Argument3 = 9;
+
+    public static double getSptTable1Argument1() {
+        return sptTable1Argument1;
+    }
+
+    public static void setSptTable1Argument1(double sptTable1Argument1) {
+        ConfigurationManager.sptTable1Argument1 = sptTable1Argument1;
+    }
+
+    public static double getSptTable1Argument2() {
+        return sptTable1Argument2;
+    }
+
+    public static void setSptTable1Argument2(double sptTable1Argument2) {
+        ConfigurationManager.sptTable1Argument2 = sptTable1Argument2;
+    }
+
+    public static double getSptTable1Argument3() {
+        return sptTable1Argument3;
+    }
+
+    public static void setSptTable1Argument3(double sptTable1Argument3) {
+        ConfigurationManager.sptTable1Argument3 = sptTable1Argument3;
+    }
+
+    public static double getSptTable1Argument4() {
+        return sptTable1Argument4;
+    }
+
+    public static void setSptTable1Argument4(double sptTable1Argument4) {
+        ConfigurationManager.sptTable1Argument4 = sptTable1Argument4;
+    }
+
+    public static double getSptTable1Argument5() {
+        return sptTable1Argument5;
+    }
+
+    public static void setSptTable1Argument5(double sptTable1Argument5) {
+        ConfigurationManager.sptTable1Argument5 = sptTable1Argument5;
+    }
+
+    public static double getSptTable2Argument1() {
+        return sptTable2Argument1;
+    }
+
+    public static void setSptTable2Argument1(double sptTable2Argument1) {
+        ConfigurationManager.sptTable2Argument1 = sptTable2Argument1;
+    }
+
+    public static double getSptTable2Argument2() {
+        return sptTable2Argument2;
+    }
+
+    public static void setSptTable2Argument2(double sptTable2Argument2) {
+        ConfigurationManager.sptTable2Argument2 = sptTable2Argument2;
+    }
+
+    public static double getSptTable2Argument3() {
+        return sptTable2Argument3;
+    }
+
+    public static void setSptTable2Argument3(double sptTable2Argument3) {
+        ConfigurationManager.sptTable2Argument3 = sptTable2Argument3;
+    }
+
+    public static double getSptTable3Argument1() {
+        return sptTable3Argument1;
+    }
+
+    public static void setSptTable3Argument1(double sptTable3Argument1) {
+        ConfigurationManager.sptTable3Argument1 = sptTable3Argument1;
+    }
+
+    public static double getSptTable3Argument2() {
+        return sptTable3Argument2;
+    }
+
+    public static void setSptTable3Argument2(double sptTable3Argument2) {
+        ConfigurationManager.sptTable3Argument2 = sptTable3Argument2;
+    }
+
+    public static double getSptTable3Argument3() {
+        return sptTable3Argument3;
+    }
+
+    public static void setSptTable3Argument3(double sptTable3Argument3) {
+        ConfigurationManager.sptTable3Argument3 = sptTable3Argument3;
+    }
+
+    public static double getDstTable1_63_5Argument1() {
+        return dstTable1_63_5Argument1;
+    }
+
+    public static void setDstTable1_63_5Argument1(double dstTable1_63_5Argument1) {
+        ConfigurationManager.dstTable1_63_5Argument1 = dstTable1_63_5Argument1;
+    }
+
+    public static double getDstTable1_63_5Argument2() {
+        return dstTable1_63_5Argument2;
+    }
+
+    public static void setDstTable1_63_5Argument2(double dstTable1_63_5Argument2) {
+        ConfigurationManager.dstTable1_63_5Argument2 = dstTable1_63_5Argument2;
+    }
+
+    public static double getDstTable1_63_5Argument3() {
+        return dstTable1_63_5Argument3;
+    }
+
+    public static void setDstTable1_63_5Argument3(double dstTable1_63_5Argument3) {
+        ConfigurationManager.dstTable1_63_5Argument3 = dstTable1_63_5Argument3;
+    }
+
+    public static double getDstTable1_63_5Argument4() {
+        return dstTable1_63_5Argument4;
+    }
+
+    public static void setDstTable1_63_5Argument4(double dstTable1_63_5Argument4) {
+        ConfigurationManager.dstTable1_63_5Argument4 = dstTable1_63_5Argument4;
+    }
+
+    public static double getDstTable1_120_Argument1() {
+        return dstTable1_120_Argument1;
+    }
+
+    public static void setDstTable1_120_Argument1(double dstTable1_120_Argument1) {
+        ConfigurationManager.dstTable1_120_Argument1 = dstTable1_120_Argument1;
+    }
+
+    public static double getDstTable1_120_Argument2() {
+        return dstTable1_120_Argument2;
+    }
+
+    public static void setDstTable1_120_Argument2(double dstTable1_120_Argument2) {
+        ConfigurationManager.dstTable1_120_Argument2 = dstTable1_120_Argument2;
+    }
+
+    public static double getDstTable1_120_Argument3() {
+        return dstTable1_120_Argument3;
+    }
+
+    public static void setDstTable1_120_Argument3(double dstTable1_120_Argument3) {
+        ConfigurationManager.dstTable1_120_Argument3 = dstTable1_120_Argument3;
+    }
+
+    public static double getDstTable1_120_Argument4() {
+        return dstTable1_120_Argument4;
+    }
+
+    public static void setDstTable1_120_Argument4(double dstTable1_120_Argument4) {
+        ConfigurationManager.dstTable1_120_Argument4 = dstTable1_120_Argument4;
+    }
+
+    public static double getDstTable1_120_Argument5() {
+        return dstTable1_120_Argument5;
+    }
+
+    public static void setDstTable1_120_Argument5(double dstTable1_120_Argument5) {
+        ConfigurationManager.dstTable1_120_Argument5 = dstTable1_120_Argument5;
+    }
+
+    public static double getDstTable2_63_5Argument2() {
+        return dstTable2_63_5Argument2;
+    }
+
+    public static void setDstTable2_63_5Argument2(double dstTable2_63_5Argument2) {
+        ConfigurationManager.dstTable2_63_5Argument2 = dstTable2_63_5Argument2;
+    }
+
+    public static double getDstTable2_63_5Argument3() {
+        return dstTable2_63_5Argument3;
+    }
+
+    public static void setDstTable2_63_5Argument3(double dstTable2_63_5Argument3) {
+        ConfigurationManager.dstTable2_63_5Argument3 = dstTable2_63_5Argument3;
+    }
+
+    public static double getDstTable2_63_5Argument4() {
+        return dstTable2_63_5Argument4;
+    }
+
+    public static void setDstTable2_63_5Argument4(double dstTable2_63_5Argument4) {
+        ConfigurationManager.dstTable2_63_5Argument4 = dstTable2_63_5Argument4;
+    }
+
+    public static double getDstTable2_63_6Argument1() {
+        return dstTable2_63_6Argument1;
+    }
+
+    public static void setDstTable2_63_6Argument1(double dstTable2_63_6Argument1) {
+        ConfigurationManager.dstTable2_63_6Argument1 = dstTable2_63_6Argument1;
+    }
+
+    public static double getDstTable2_63_6Argument2() {
+        return dstTable2_63_6Argument2;
+    }
+
+    public static void setDstTable2_63_6Argument2(double dstTable2_63_6Argument2) {
+        ConfigurationManager.dstTable2_63_6Argument2 = dstTable2_63_6Argument2;
+    }
+
+    public static double getDstTable2_63_6Argument3() {
+        return dstTable2_63_6Argument3;
+    }
+
+    public static void setDstTable2_63_6Argument3(double dstTable2_63_6Argument3) {
+        ConfigurationManager.dstTable2_63_6Argument3 = dstTable2_63_6Argument3;
+    }
+
+    public static double getDstTable2_63_7Argument1() {
+        return dstTable2_63_7Argument1;
+    }
+
+    public static void setDstTable2_63_7Argument1(double dstTable2_63_7Argument1) {
+        ConfigurationManager.dstTable2_63_7Argument1 = dstTable2_63_7Argument1;
+    }
+
+    public static double getDstTable2_63_7Argument2() {
+        return dstTable2_63_7Argument2;
+    }
+
+    public static void setDstTable2_63_7Argument2(double dstTable2_63_7Argument2) {
+        ConfigurationManager.dstTable2_63_7Argument2 = dstTable2_63_7Argument2;
+    }
+
+    public static double getDstTable2_63_7Argument3() {
+        return dstTable2_63_7Argument3;
+    }
+
+    public static void setDstTable2_63_7Argument3(double dstTable2_63_7Argument3) {
+        ConfigurationManager.dstTable2_63_7Argument3 = dstTable2_63_7Argument3;
+    }
+}
